ae65137bf60fbbd7cce64533207c9cac984071dd
rachitrai99 <rachit.rai_cs18@gla.ac.in>
2020-11-26 18:06:08 +0530

    files rearranged

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+// dbPassword = 'mongodb+srv://rachit'+ encodeURIComponent('Jaypee01') + '@Cluster0.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: 'mongodb+srv://rachit:Jaypee01@cluster0.9pvci.mongodb.net/test?retryWrites=true&w=majority'
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,43 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
-router.get('/',(req, res) => res.render('welcome'));
+// Welcome Page
+ //router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// router.get('/', (req, res,next)=>{
+//     res.sendFile(path.join(__dirname, 'views', 'hoga.html'));
+// });
+
+
+
+router.use(express.static('views'));
+router.get('/',forwardAuthenticated,function(req,res){
+  res.sendFile(path.join(__dirname,'views','/index.html'));
+  //__dirname : It will resolve to your project folder.s
+});  
+
+router.get('/welcome', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+router.use(express.static('views'));
+router.get('/cart',forwardAuthenticated,function(req,res){
+  res.sendFile(path.join(__dirname,'views','/cart.html'));
+  //__dirname : It will resolve to your project folder.s
+}); 
+
+
+/*
+router.get('/menu',forwardAuthenticated,function(req,res){
+  res.sendFile(path.join(__dirname,'views','/menu.html'));
+  //__dirname : It will resolve to your project folder.s
+});  */  
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    user: req.user
+  })
+);
 
 module.exports = router;
